0c0ab070b2a5a851f0e181b645fb963104ba6cda
viniciustec <vinialvesrocha@gmail.com>
2021-04-30 09:16:54 -0300

    Exercicio1 - trazendo apenas 1 pokemon de cada vez e criando botao

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
-
 class Pokedex extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            indexPokemon: 0
+        }
+    }
+    handleClick = () => {
+        this.setState((indexAnterior, _props) => {
+            if (this.state.indexPokemon === pokemons.length - 1) {
+                return ({
+                    indexPokemon: 0
+                }); 
+            }
+            return ({
+            indexPokemon: indexAnterior.indexPokemon +1
+        });
+    })
+    }
     render() {
+        const { pokemons } = this.props;
         return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
+            <>
+                <div className="pokedex">
+                    <Pokemon key={pokemons[this.state.indexPokemon].id} pokemon={pokemons[this.state.indexPokemon]} />
+                </div>
+                <button onClick={this.handleClick}>Próximo</button>
+            </>
         );
     }
 }
-
 export default Pokedex;
